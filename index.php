490d04399a432064a23d9f7268e5fba403168ccf
Peter Costa <petersvc@outlook.com>
2018-11-22 14:08:14 -0300

    flex bootstrap

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<?php get_header(); ?>
-
-<div id="ttr_main" class="row">
-    <div id="ttr_content" class="col-lg-8 col-sm-8 col-md-8 col-xs-12">
-        <div class="row">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-                <h1><?php the_title(); ?></h1>
-                <h4>Postado em <?php the_time('d, m, Y') ?></h4>
-                <p><?php the_content('Read more ...'); ?></p>
-            </div>
-            <?php endwhile; else: ?>
-            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-            <?php endif; ?>
+<div id="ttr_main" class="container-fluid">
+    <div class="row-fluid"><?php get_header(); ?></div>
+    <div id="coisa" class="row">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div class="col-sm-5">
+            <h1><?php the_title(); ?></h1>
+            <h4>Postado em <?php the_time('d, m, Y') ?></h4>
+            <p><?php the_content('Read more ...'); ?></p>
         </div>
+        <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+        <div class="col-sm-2"><?php get_sidebar(); ?></div> 
     </div>
-    <?php get_sidebar(); ?>
+</div>
+       
 </div>
 
 <?php get_footer(); ?>
